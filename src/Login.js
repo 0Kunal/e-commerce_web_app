45ae6438b7199c01d6e4cb6516b85f3cb0871b36
Aritra Debnath <119656726+Aritra004@users.noreply.github.com>
2023-08-24 23:19:20 +0530

    Add files via upload

--- src/Login.js
+++ src/Login.js
@@ -1,95 +1,69 @@
-import React from 'react';
-import {Button, TextField, Grid, Typography} from '@mui/material';
-import { makeStyles } from "@mui/styles";
+import React from "react";
+import {
+  TextField,
+  Button,
+  Typography,
+  Grid,
+  Box,
+  Radio,
+  RadioGroup,
+  FormControlLabel,
+} from "@mui/material";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 
-const useStyles = makeStyles({
-  root: {
-    height: '100vh',
-  },
-  left: {
-    backgroundColor: 'green',
-    color: 'white',
-    padding: '40px',
-  },
-  right: {
-    backgroundColor: 'white',
-    padding: '40px',
-  },
-  logo: {
-    fontWeight: 'bold',
-    fontSize: '2rem',
-    paddingBottom: '10px',
-  },
-  form: {
-    marginTop: '40px',
-  },
-  input: {
-    marginBottom: '20px',
-  },
-  button: {
-    marginTop: '20px',
-    marginBottom: '20px',
-  },
-  googleButton: {
-    backgroundColor: 'brown',
-    color: 'white',
-    '&:hover': {
-      backgroundColor: 'red',
-    },
-  },
-});
-
-const Login = () => {
-
-  const classes = useStyles();
-
-  const [email, setEmail] = React.useState('');
-  const [password, setPassword] = React.useState('');
-
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
-  };
-
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  };
-
-  const handleLoginClick = () => {
-    console.log('Login with email and password');
-  };
-
-  const handleGoogleSigninClick = () => {
-    console.log("Sign in with Google");
-  }
-
+const Register = () => {
   return (
-    <>
-      <Grid container classname={classes.root}>
-        <Grid item xs={12} sm={6} className={classes.left}>
-          <Typography variant="h4" align="center">
-            A picture will go here
-          </Typography>
-          <Typography variant="h6" align="center">
-            A Picture will ho here
-          </Typography>
+    <Box padding={2}>
+      <Grid container borderRadius={3} overflow={"hidden"}>
+        <Grid item xs={12} sm={7} sx={{ backgroundColor: "blue" }}>
+          <img
+            src="https://www.hktechit.com/wp-content/uploads/2020/01/Custom-ECommerce-Website-Development-vector-image.png"
+            alt="login"
+            objectFit="cover"
+            style={{ maxWidth: "100%" }}
+          />
         </Grid>
-        <Grid item xs={12} sm={6} className={classes.right}>
-          <Typography  variant="h4" className={classes.logo}>
-            Sign in to continue shopping
-          </Typography>
-          <form classname={classes.form} noValidate>
-            <TextField className={classes.input} label="Email" type="email" value={email} onChange={handleEmailChange} fullWidth required />
-            <TextField className={classes.input} label="Password" type="password" value={password} onChange={handlePasswordChange} fullWidth required />
-            <Button className={classes.button} variant="contained" color="primary" onClick={handleLoginClick} fullWidth>Log In</Button>
-            <Typography variant="body2" align="center">
-              Forgot Password?
+        <Grid item xs={12} sm={5} sx={{ backgroundColor: "whitesmoke" }}>
+          <Box padding={2}>
+            <Typography variant="h4" gutterBottom textAlign={"center"}>
+              Sign-In to start shopping
             </Typography>
-            <Button className={`${classes.button} ${classes.googleButton}`} variant="contained" onClick={handleGoogleSigninClick} fullWidth>Sign In With Google</Button>
-          </form>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  label="Email"
+                  size="small"
+                  variant="outlined"
+                  fullWidth
+                  required
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  label="Password"
+                  size="small"
+                  variant="outlined"
+                  fullWidth
+                  required
+                />
+              </Grid>
+
+              <Grid item xs={6} textAlign={"end"}>
+                <Button variant="contained" color="error">
+                  Reset
+                </Button>
+              </Grid>
+              <Grid item xs={6}>
+                <Button variant="contained" color="success">
+                  Login
+                </Button>
+              </Grid>
+            </Grid>
+          </Box>
         </Grid>
       </Grid>
-    </>
+    </Box>
   );
 };
 
-export default Login
+export default Register;
